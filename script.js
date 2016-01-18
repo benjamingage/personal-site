eae36fc6dbda3c11d9aebbb68242126e58c73640
benjamin gage <ben.gage@carrotcreative.com>
2016-01-18 15:30:07 -0500

    small update
    new oliver image
    lots of css for the tooltips
    tooltips

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = document.getElementsByclassName("name");
+    var tp = document.getElementsByclassName("tooltip");
+
+    function show(){
+        tp.style.display="block";
+    }
+    function hide(){
+        tp.style.display="";
+    }
