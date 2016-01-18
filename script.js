339095f185b864fa37715754b5cbc5e9d5af5761
benjamin gage <ben.gage@carrotcreative.com>
2016-01-18 16:06:52 -0500

    weird position issue
    changed position on the .social-links class to absolute because i had
    some weird spacing issues when i uploaded to the server.

--- script.js
+++
@@ -1,9 +0,0 @@
-var name = document.getElementsByclassName("name");
-    var tp = document.getElementsByclassName("tooltip");
-
-    function show(){
-        tp.style.display="block";
-    }
-    function hide(){
-        tp.style.display="";
-    }
